f44578e510116c068ce2aeb87ab99f952c970d44
vadim.babii <vadim.babii@theopentag.com>
2022-12-12 14:08:36 +0200

    HW-1 has been done

---
+++ HW-1/index.js
@@ -0,0 +1,99 @@
+const APPLES = 15.678;
+let ORANGES = 123.965;
+let BANANAS = 90.2345;
+document.writeln("Завдання категорії Base", "<br />");
+
+document.writeln(
+  "Максимальне значення: ",
+  Math.max(APPLES, ORANGES, BANANAS),
+  ".",
+  "<br />"
+);
+
+document.writeln(
+  "Мінімальне значення: ",
+  Math.min(APPLES, ORANGES, BANANAS),
+  ".",
+  "<br />"
+);
+
+const FRUITES_SUM = APPLES + ORANGES + BANANAS;
+document.writeln("Сума всіх товарів: ", FRUITES_SUM, ".", "<br />");
+
+document.writeln(
+  "Сума всіх цілих вартостей: ",
+  Math.floor(APPLES) + Math.floor(ORANGES) + Math.floor(BANANAS),
+  ".",
+  "<br />"
+);
+
+document.writeln(
+  "Сума всіх товарів, заокруглена до сотень: ",
+  Math.ceil(FRUITES_SUM / 100) * 100,
+  ".",
+  "<br />"
+);
+
+document.writeln(
+  "Чи є ціла сума всіх вартостей парною: ",
+  Math.floor(FRUITES_SUM) % 2 === 0,
+  "<br />"
+);
+
+document.writeln(
+  "Здача при покупці на 500 грн.: ",
+  500 - FRUITES_SUM,
+  ".",
+  "<br />"
+);
+
+document.writeln("Середня ціна: ", (FRUITES_SUM / 3).toFixed(2), ".", "<br />");
+
+const DISCOUNT = Math.random().toFixed(2);
+document.writeln(
+  "Сума до оплати: ",
+  (FRUITES_SUM - FRUITES_SUM * DISCOUNT).toFixed(2),
+  ", знижка складає: ",
+  DISCOUNT * 100,
+  "% - за умови покупки всіх трьох товарі",
+  ".",
+  "<br />"
+);
+
+if (DISCOUNT > 0.5) {
+  const PROFIT =
+    "Втрачена вигода: " +
+    (FRUITES_SUM * DISCOUNT - FRUITES_SUM / 2).toFixed(2) +
+    "<br />";
+} else if (DISCOUNT < 0.5) {
+  const PROFIT =
+    "Прибуток склав: " +
+    (FRUITES_SUM - FRUITES_SUM * DISCOUNT - FRUITES_SUM / 2).toFixed(2) +
+    "<br />";
+} else {
+  const PROFIT = "Товар продано по собівартості" + "<br />";
+}
+document.writeln(PROFIT);
+
+document.writeln("Завдання категорії Advanced", "<br />");
+document.writeln(`
+    Максимальне значення: ${Math.max(APPLES, ORANGES, BANANAS)}.<br \/>
+    Мінімальне значення: ${Math.min(APPLES, ORANGES, BANANAS)}.<br \/>
+    Сума всіх товарів: ${FRUITES_SUM}.<br \/>
+    Сума всіх цілих вартостей: ${
+      Math.floor(APPLES) + Math.floor(ORANGES) + Math.floor(BANANAS)
+    }.<br \/>
+    Сума всіх товарів, заокруглена до сотень: ${
+      Math.ceil(FRUITES_SUM / 100) * 100
+    }.<br \/>
+    Чи є ціла сума всіх вартостей парною: ${
+      Math.floor(FRUITES_SUM) % 2 === 0
+    }.<br \/>
+    Здача при покупці на 500 грн.: ${500 - FRUITES_SUM}.<br \/>
+    Середня ціна: ${(FRUITES_SUM / 3).toFixed(2)}.<br \/>
+    Сума до оплати: ${(FRUITES_SUM - FRUITES_SUM * DISCOUNT).toFixed(2)}.<br \/>
+    Знижка складає: ${
+      DISCOUNT * 100
+    }% - за умови покупки всіх трьох товарі.<br \/>
+    ${PROFIT}
+`);
